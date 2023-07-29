e03bd441fdc9cf97b6d34d8f7d1bae1f470ffd46
Andrew Maddox <andrewmaddox@Andrews-MacBook-Pro-2.local>
2023-07-29 16:08:54 +0100

    test for AWS git pull

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is a test of my understanding of Github</p>
+        <p>This is a test of my website working</p>
+        <p>It is working</p>
+        <p>Unfortunately, the fucking AWS version isn't</p>
       </header>
     </div>
   );
