53cb0a495fa9635d68343a0338913b0af717e669
Andrew Maddox <andrewmaddox@Andrews-MacBook-Pro-2.local>
2023-07-29 14:33:46 +0100

    Line deletion

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Thanks for your help Nick, but this is enough for tonight</p>
         <p>This is a test of my understanding of Github</p>
       </header>
     </div>
